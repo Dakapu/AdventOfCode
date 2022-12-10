10a32dbcb8898f9e842ce8e6b5423c456cec2232
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-10 16:02:59 +0100

    hardcoded approach
    less elegant but was deemed correct by the platform
    TODO: change previous code to get result 12.156

--- 02/RockPaperScissors.js
+++ 02/RockPaperScissors.js
@@ -31,75 +31,42 @@ processedData.forEach((element) => {
   //split every element into an array with two items
   //pair[0] = oponent
   //pair[1] = outcome
-  let pair = element.split(" ");
+  let pair = element.replace(" ", "");
 
-  //initilize Scores
+  switch (pair) {
+    case "AX":
+      finalScore += 4;
+      break;
+    case "AY":
+      finalScore += 8;
+      break;
+    case "AZ":
+      finalScore += 3;
+      break;
 
-  //save oponentScore to pitch it against the player
-  let oponentTool = determineScore(pair[0]);
+    case "BX":
+      finalScore += 1;
+      break;
+    case "BY":
+      finalScore += 5;
+      break;
+    case "BZ":
+      finalScore += 9;
+      break;
 
-  //determine the outcome
-  let outcome = determineScore(pair[1]);
+    case "CX":
+      finalScore += 7;
+      break;
+    case "CY":
+      finalScore += 2;
+      break;
+    case "CZ":
+      finalScore += 6;
+      break;
 
-  //determine playerScore by finding out what action to use
-  let playerScore = determinePlayerTool(outcome, oponentTool);
-
-  //get the strategy-guide outcome
-  let currentScore = outcome + playerScore;
-
-  console.log("____");
-  console.log("The outcome SHOULD be: ", pair[1]);
-  console.log("The Oponent Score is: ", oponentTool);
-  console.log("The Player Score is: ", playerScore);
-  console.log("The current score is: ", currentScore);
-
-  //update the final score
-  finalScore += currentScore;
+    default:
+      break;
+  }
 });
 
-console.log("Final Score: ", finalScore);
-
-function determineScore(contestant) {
-  switch (contestant) {
-    //Rock
-    case "A":
-      return 1;
-    //Paper
-    case "B":
-      return 2;
-    //Scissors
-    case "C":
-      return 3;
-
-    //loss
-    case "X":
-      return 0;
-    //win
-    case "Y":
-      return 6;
-    //draw
-    case "Z":
-      return 3;
-    default:
-      break;
-  }
-}
-
-function determinePlayerTool(currentScore, oponentTool) {
-  switch (currentScore) {
-    //LOSS
-    case 0:
-      //the player needs to be one score below the oponent UNLESS the oponent plays a Rock (1) then the player needs to play Scissors (3)
-      return oponentTool == 1 ? 3 : oponentTool - 1;
-    //DRAW
-    case 3:
-      //since a draw always requires playing the SAME as the oponenet, just add its value
-      return oponentTool;
-    //WIN
-    case 6:
-      //the player needs to be one above the oponent UNLESS the oponent plays Scissors (3), then the player needs to play Rock (1)
-      return oponentTool == 3 ? 1 : oponentTool + 1;
-    default:
-      break;
-  }
-}
+console.log(finalScore);
