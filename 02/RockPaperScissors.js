41a437b8d0e8adeeda80ec725af1e682e3ffa479
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-10 12:47:52 +0100

    Setup

---
+++ 02/RockPaperScissors.js
@@ -0,0 +1,23 @@
+/*
+
+Strategy Guide
+
+                    Scores
+
+*Oponent*
+    A = Rock        1
+    B = Paper       2
+    C = Scissors    3
+
+*Player*
+    X = Rock        1
+    Y = Paper       2
+    Z = Scissors    3
+
+    
+Win                 6
+Draw                3
+Loss                0
+
+
+*/
