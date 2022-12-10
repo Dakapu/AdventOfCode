7c9476748aec76af6bf78f23f24228278c117963
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-10 13:27:30 +0100

    determine scores and winner

--- 02/RockPaperScissors.js
+++ 02/RockPaperScissors.js
@@ -31,13 +31,77 @@ var data = fs.readFileSync("input.txt").toString();
 //process the data by splitting the textfile on empty lines and create array entries for each pair
 var processedData = data.split(/\r\n/g);
 //initilize final score
-var result = 0;
+var finalScore = 0;
 
 processedData.forEach((element) => {
+  //split every element into an array with two items
+  //pair[0] = oponent
+  //pair[1] = player
   let pair = element.split(" ");
-  //initilize current score
+
+  //initilize currentScore
+  let playerScore = 0;
+  let oponentScore = 0;
+
+  //TO-DO: Refactor!
 
   //switch over pairs to calculate score
+  switch (pair[0]) {
+    case "A":
+      //determine playerScore by finding out what tool they used
+      playerScore = determineTool(pair[1]);
+      //save oponentScore to pitch it against the player
+      oponentScore = determineTool(pair[0]);
+      //compare the scores and see who won
+      //if the playerScore is greater than oponent's Score, then add the playerTool plus the outcome to finalscore
+      //if it's a draw add the playerScore and the draw-score
+      playerScore > oponentScore
+        ? (finalScore += playerScore + 6)
+        : playerScore == oponentScore
+        ? (finalScore += playerScore + 3)
+        : finalScore;
+      console.log("Player Score: ", playerScore);
+      console.log("Oponent Score: ", oponentScore);
+      break;
 
-  //add currentscore to final score
+    case "B":
+      playerScore = determineTool(pair[1]);
+      oponentScore = determineTool(pair[0]);
+      playerScore > oponentScore ? (finalScore += playerScore) : finalScore;
+
+      break;
+    case "C":
+      playerScore = determineTool(pair[1]);
+      oponentScore = determineTool(pair[0]);
+      playerScore > oponentScore ? (finalScore += playerScore) : finalScore;
+
+      break;
+
+    default:
+      break;
+  }
+
+  //add currentScore to final score
 });
+
+function determineTool(player) {
+  switch (player) {
+    //use fall-through to cover for the usual "or" statement
+    case "X":
+    case "A":
+      return 1;
+
+    case "Y":
+    case "B":
+      return 2;
+
+    case "Z":
+    case "C":
+      return 3;
+
+    default:
+      break;
+  }
+}
+
+console.log("Final Score: ", finalScore);
