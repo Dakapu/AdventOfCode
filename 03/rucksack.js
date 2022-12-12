89c046faac1610b5af0070543fc45cd2ec338e35
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-12 14:11:50 +0100

    clean-up

--- 03/rucksack.js
+++ 03/rucksack.js
@@ -36,8 +36,6 @@ function checkBadge(team) {
   return toPriority(badge);
 }
 
-console.log(result);
-
 function toPriority(item) {
   //Lowercase item types a through z have priorities 1 through 26.
   //Uppercase item types A through Z have priorities 27 through 52.
@@ -51,3 +49,5 @@ function toPriority(item) {
     return item.charCodeAt(0) - 96;
   }
 }
+
+console.log(result);
