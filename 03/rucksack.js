3e2bef4df6f4f3d570af841c702dcf4b79a0b17f
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-12 14:11:04 +0100

    solve follow-up puzzle

--- 03/rucksack.js
+++ 03/rucksack.js
@@ -2,8 +2,10 @@
 var fs = require("fs");
 
 //get data from external textfile
-var data = fs.readFileSync("example.txt").toString();
+var data = fs.readFileSync("input.txt").toString();
 var processedData = data.split(/\r\n/g);
+
+var result = 0;
 
 //Split data into chunks of three
 const chunkSize = 3;
@@ -12,35 +14,27 @@ const chunkSize = 3;
 for (let i = 0; i < processedData.length; i += chunkSize) {
   //form array dependent on chunk size
   const chunk = processedData.slice(i, i + chunkSize);
-
-  console.log(chunk);
+  //method to check for duplicates
+  const badge = checkBadge(chunk);
+  //add to the overall result
+  result += badge;
 }
 
-var result = 0;
+//take the three rucksacks
+function checkBadge(team) {
+  //save the three single rucksacks as arrays made from their respective substrings
+  let firstElf = [...team[0].substring(0)];
+  let secondElf = [...team[1].substring(0)];
+  let thirdElf = [...team[2].substring(0)];
 
-//Data = new line => new rucksack
-processedData.forEach((rucksack) => {
-  //each rucksack will have two equal sized compartments => half the line
-  let numberOfItems = rucksack.length;
+  //check the first two rucksacks for double items and combine them into making a "helper" Rucksack to validate against
+  const combinedRucksacks = firstElf.filter((item) => secondElf.includes(item));
+  //check the third rucksack for doubled items found in combined Rucksacks
+  const badge = combinedRucksacks.filter((item) => thirdElf.includes(item))[0];
 
-  //save the halves
-  let halfOfItems = numberOfItems / 2;
-  //save the substrings as array via spread-operator
-  let firstHalf = [...rucksack.substring(0, halfOfItems)];
-  let secondHalf = [...rucksack.substring(halfOfItems)];
-
-  //compare halves with each other
-  //save only the first occurance of the doubled item to variable
-  //otherwise the output would be ['s', 's', 's']
-  //doubledItem is a string
-  const doubledItem = firstHalf.filter((item) => secondHalf.includes(item))[0];
-
-  //convert item to priority
-  let itemPriority = toPriority(doubledItem);
-
-  //add the priority to the overall result
-  result += itemPriority;
-});
+  //return the badge after converting it to a priority
+  return toPriority(badge);
+}
 
 console.log(result);
 
