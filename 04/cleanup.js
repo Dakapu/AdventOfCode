44a100c08a03aa812ab1d474f0c24c1c356f69d1
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-13 13:23:10 +0100

    Lay out numbers to compare

--- 04/cleanup.js
+++ 04/cleanup.js
@@ -2,5 +2,31 @@
 var fs = require("fs");
 
 //get data from external textfile
-var data = fs.readFileSync("input.txt").toString();
+var data = fs.readFileSync("example.txt").toString();
 var processedData = data.split(/\r\n/g);
+
+processedData.forEach((cleaningPair) => {
+  //remove dash and split the pair in two parts
+  //careful: Pair contains TWO STRINGS not NUMBERS
+  let pair = cleaningPair.replace(/-/g, "").split(",");
+
+  //function to retreive the whole set of numbers from the one number given
+  //convert to number first
+  let firstElf = layOutNumbers(pair[0]);
+  let secondElf = layOutNumbers(pair[1]);
+
+  console.log();
+});
+
+function layOutNumbers(num) {
+  let result = num[0];
+  let start = num[0];
+  let end = num[1];
+
+  for (let i = start; i <= end; i++) {
+    //count up to end and add the number to result
+    result += i;
+  }
+  //chop of first element of result as it had to be initilised first
+  console.log(result.substring(1));
+}
