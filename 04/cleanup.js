c5b628aed765a16e745b1662ca75018389b03814
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-12-13 14:26:56 +0100

    replace string comparision with numberrange comparision

--- 04/cleanup.js
+++ 04/cleanup.js
@@ -2,31 +2,33 @@
 var fs = require("fs");
 
 //get data from external textfile
-var data = fs.readFileSync("example.txt").toString();
+var data = fs.readFileSync("input.txt").toString();
 var processedData = data.split(/\r\n/g);
+
+var doubles = 0;
 
 processedData.forEach((cleaningPair) => {
   //remove dash and split the pair in two parts
   //careful: Pair contains TWO STRINGS not NUMBERS
-  let pair = cleaningPair.replace(/-/g, "").split(",");
+  let pair = cleaningPair.split(",");
 
   //function to retreive the whole set of numbers from the one number given
-  //convert to number first
-  let firstElf = layOutNumbers(pair[0]);
-  let secondElf = layOutNumbers(pair[1]);
+  let firstElf = pair[0].split("-");
+  //save the starting and end points of the workload of each elf
+  let firstElfStart = Number(firstElf[0]);
+  let firstElfFinish = Number(firstElf[1]);
 
-  console.log();
+  let secondElf = pair[1].split("-");
+  let secondElfStart = Number(secondElf[0]);
+  let secondElfFinish = Number(secondElf[1]);
+
+  //compare the two workloads if either is contained in the other and if so increase doubles
+  if (
+    (firstElfStart <= secondElfStart && firstElfFinish >= secondElfFinish) ||
+    (firstElfStart >= secondElfStart && firstElfFinish <= secondElfFinish)
+  ) {
+    doubles++;
+  }
 });
 
-function layOutNumbers(num) {
-  let result = num[0];
-  let start = num[0];
-  let end = num[1];
-
-  for (let i = start; i <= end; i++) {
-    //count up to end and add the number to result
-    result += i;
-  }
-  //chop of first element of result as it had to be initilised first
-  console.log(result.substring(1));
-}
+console.log(doubles);
